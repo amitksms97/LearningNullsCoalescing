f9d76b464e0a94c5531db9e2c0f75819b5fc884a
amitksms97 <amitksms97@gmail.com>
2021-05-01 18:24:34 +0530

    Add project files.

---
+++ LearningNulls/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LearningNulls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = null; // "hello";
+            var s2 = s ?? "default";
+
+            int ?ss = null; // 1;
+            int ss2 = ss ?? 2;
+
+            Console.WriteLine(s2);
+            Console.WriteLine(ss2);
+
+        }
+    }
+}
